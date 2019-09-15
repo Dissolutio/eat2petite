64be0e97da2988859a7fcbe489daeb08b1ba06e0
John Moen <entity.john@gmail.com>
2019-09-15 12:46:23 -0500

    add create post form component

---
+++ src/components/forms/UserCreatePostForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useDataContext } from '../../modules/hooks/useDataContext'
+import { useAuthUserContext } from '../../firebase'
+
+export default function UserCreatePostForm() {
+	const { createUserPost } = useDataContext()
+	const { user } = useAuthUserContext()
+	const createPostOnSubmit = event => {
+		event.preventDefault()
+		console.log(event.target)
+		let newPost = {
+			author: user.uid,
+			createdAt: event.target.createdAt.value,
+			postData: {
+				quantity: event.target.quantity.value,
+				quantityUnits: event.target.units.value,
+			},
+		}
+		createUserPost(newPost)
+	}
+	return (
+		<div>
+			<form onSubmit={createPostOnSubmit}>
+				<input name="createdAt" type="date" />
+				<input name="quantity" type="text" placeholder="quantity" />
+				<select name="units" defaultValue="ounces">
+					<option value="cups">Cups</option>
+					<option value="ounces">Ounces</option>
+					<option value="liters">Liters</option>
+				</select>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	)
+}
--- src/components/shared/UserDashboard.jsx
+++ src/components/shared/UserDashboard.jsx
@@ -3,6 +3,7 @@ import { useDataContext } from '../../modules/hooks/useDataContext'
 
 // import UserPostsList from './UserPostsList'
 import UserChallengesList from './UserChallengesList'
+import UserCreatePostForm from '../forms/UserCreatePostForm'
 
 function filterAppDataUser(data) {
 	return { ...data }
@@ -10,13 +11,15 @@ function filterAppDataUser(data) {
 
 export default function UserDashboard() {
 	const { appData } = useDataContext()
+
 	const userAppData = filterAppDataUser(appData)
 	const { challenges } = userAppData
+
 	return (
 		<div>
 			<h1>User Dashboard</h1>
+			<UserCreatePostForm />
 			<UserChallengesList challenges={challenges} />
-			{/* <UserPostsList posts={posts} /> */}
 		</div>
 	)
 }
