5c8bdae134eb3c2c1992657b2a04f3f7f5598678
John Moen <entity.john@gmail.com>
2019-09-18 22:27:41 -0500

    build out user challenge detail page

--- src/components/user/UserChallengeDetail.jsx
+++ src/components/user/UserChallengeDetail.jsx
@@ -1,5 +1,34 @@
 import React from 'react'
+import { Badge, Col, Card, CardHeader, CardBody, CardFooter, CardTitle, CardText } from 'reactstrap'
+import { ChallengeCardLink } from '../navigation/Links'
+import { useDataContext } from '../../contexts/useDataContext'
 
-export default function UserChallengeDetail() {
-	return <div>UserChallengeDetail</div>
+export default function UserChallengeDetail(props) {
+	const { id } = props.match.params
+	const { appData } = useDataContext()
+	const { challenges } = appData
+	const challenge = challenges[id]
+	const { uid, challengeName, description, formulaForTarget, units } = challenge
+	return (
+		challenge && (
+			<Col sm="6">
+				<Card color="primary" outline key={uid} body>
+					<CardHeader>
+						<CardTitle>
+							<h3>{challengeName}</h3>
+						</CardTitle>
+					</CardHeader>
+					<CardBody>
+						<CardText>Description</CardText>
+						<CardText>{description}</CardText>
+						<CardText>{`Units: ${units}`}</CardText>
+					</CardBody>
+					<CardFooter>
+						<CardText>Formula For Target</CardText>
+						<CardText>{formulaForTarget}</CardText>
+					</CardFooter>
+				</Card>
+			</Col>
+		)
+	)
 }
