b50895c6503620d019812e42cf0c9b6c783c7ae0
John Moen <entity.john@gmail.com>
2019-09-19 23:58:08 -0500

    render landing page when on root URL, don't wait for authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+
 import { useAuthUserContext } from './contexts/useAuthUserContext'
 import SideNav from './components/navigation/SideNav'
 import Header from './components/layout/Header'
@@ -7,11 +9,10 @@ import AppStyle from './components/layout/AppStyle'
 import PageStyle from './components/layout/PageStyle'
 import DevConsole from './components/shared/DevConsole'
 
-function App() {
+function App(props) {
 	const { initializing } = useAuthUserContext()
-	if (initializing) {
-		// TODO make this a cool loader
-		return <h1>Initializing Authentication</h1>
+	if (initializing && props.location.pathname === '/') {
+		return <h1>Authenticating...</h1>
 	}
 	return (
 		<AppStyle>
@@ -24,4 +25,4 @@ function App() {
 		</AppStyle>
 	)
 }
-export default App
+export default withRouter(App)
