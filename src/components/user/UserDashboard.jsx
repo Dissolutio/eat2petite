d64358f848f10b271dbdbd3487889c1c5b082f6c
John Moen <entity.john@gmail.com>
2019-09-20 01:02:09 -0500

    implement new post and contest lists in UserDashboard

--- src/components/user/UserDashboard.jsx
+++ src/components/user/UserDashboard.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
+
 import { useDataContext } from '../../contexts/useDataContext'
 
+import UserCreatePostForm from '../forms/UserCreatePostForm'
+import UserContestsList from './UserContestsList'
 import UserPostsList from './UserPostsList'
 import UserChallengesList from './UserChallengesList'
-import UserCreatePostForm from '../forms/UserCreatePostForm'
-import { useAuthUserContext } from '../../contexts/useAuthUserContext'
 
 export default function UserDashboard() {
 	const { appData } = useDataContext()
-	const { user } = useAuthUserContext()
-	const { challenges, posts, users } = appData
-	const currentUser = users[user.uid]
+	const { contests, challenges, posts } = appData
 	return (
 		<div>
 			<h1>User Dashboard</h1>
+			<UserContestsList contests={contests} />
 			<UserCreatePostForm />
-			<UserPostsList posts={posts} currentUser={currentUser} />
+			<UserPostsList posts={posts} />
 			<UserChallengesList challenges={challenges} />
 		</div>
 	)
