d8c612b75f8d371f47423294e5ba9f5bfd809f7c
John Moen <entity.john@gmail.com>
2019-09-24 15:09:49 -0500

    work on user dash calendar

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -4,12 +4,14 @@ import { useDataContext } from '../../contexts/useDataContext'
 import AdminChallengesList from './AdminChallengesList'
 import AdminContestsList from './AdminContestsList'
 import DevConsole from '../shared/DevConsole'
+
 export default function AdminDashboard() {
 	const { appData } = useDataContext()
 	const { users, posts, challenges, contests } = appData
 	return (
 		<div>
 			<h2>Admin Dashboard</h2>
+			<DevConsole />
 			<AdminChallengesList challenges={challenges} />
 			<AdminContestsList contests={contests} />
 		</div>
--- src/components/user/UserDashboard.jsx
+++ src/components/user/UserDashboard.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Calendar from 'react-calendar'
 
 import { useDataContext } from '../../contexts/useDataContext'
 
@@ -12,6 +13,40 @@ import DevConsole from '../shared/DevConsole'
 // 2. Find matching post for each day
 /// 3. Display latest six days
 // 4. Display quick post form for today
+
+const challenge = () => ({
+	challengeName: 'Manage Sugar and Salt Intake',
+	description:
+		'The average diet has a huge amount of added sugar and salt, and we can benefit from monitoring and managing how much we take into our bodies.',
+	formulaForTarget: 'Some amount of weight per human',
+	uid: '-LpZo0UXFp8B7C8X5EMW',
+	units: 'weight',
+})
+const getDateArray = (start, end) => {
+	const arr = new Array()
+	const dt = new Date(start)
+	while (dt <= end) {
+		arr.push(new Date(dt))
+		dt.setDate(dt.getDate() + 1)
+	}
+	return arr
+}
+const DashboardCalendar = props => {
+	const [selectedDate, setSelectedDate] = React.useState(new Date(2019, 8, 1))
+	const changeHandler = date => setSelectedDate({ date })
+
+	return (
+		<div>
+			<Calendar
+				onChange={changeHandler}
+				value={selectedDate}
+				minDate={new Date(2019, 8, 1)}
+				maxDate={new Date(2019, 8, 30)}
+			/>
+		</div>
+	)
+}
+
 export default function UserDashboard() {
 	const { appData } = useDataContext()
 	const { contests, challenges, posts } = appData
@@ -19,6 +54,7 @@ export default function UserDashboard() {
 		<div>
 			<h1 className="text-center">User Dashboard</h1>
 			<hr />
+			<DashboardCalendar />
 			<DevConsole></DevConsole>
 			<UserContestsList contests={contests} />
 			<UserPostsList posts={posts} />
