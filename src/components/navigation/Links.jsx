fbf9fb422ef31af276663a2ad37c8439228ca917
John Moen <entity.john@gmail.com>
2019-09-30 18:25:36 -0500

    refactor and cleanup internal links components

--- src/components/navigation/Links.jsx
+++ src/components/navigation/Links.jsx
@@ -2,30 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import * as ROUTES from '../../routes'
 
-export const UserContestDetailLink = props => {
-	const { id } = props
-	return <Link to={`${ROUTES.USER_POSTS}${id}`}>{`Contest ID: ${id}`}</Link>
-}
-export const UserPostDetailLink = props => {
-	const { id } = props
-	return <Link to={`${ROUTES.USER_POSTS}${id}`}>{`Post ID: ${id}`}</Link>
-}
-export const UserChallengeDetailLink = props => {
-	const { id } = props
-	return <Link to={`${ROUTES.USER_CHALLENGES}${id}`}>{props.children}</Link>
-}
+export const UserContestDetailLink = ({id}) => <Link to={`${ROUTES.USER_POSTS}${id}`}>{`Contest ID: ${id}`}</Link>
+
+export const UserPostDetailLink = ({id}) =>  <Link to={`${ROUTES.USER_POSTS}${id}`}>{`Post ID: ${id}`}</Link>
+
+export const UserChallengeDetailLink = ({id, children}) => <Link to={`${ROUTES.USER_CHALLENGES}${id}`}>{children}</Link>
+
 export const ChallengeCardLink = ({ uid, children }) => <Link to={`${ROUTES.ADMIN_CHALLENGES}${uid}`}>{children}</Link>
 
-export const AdminUserDetailLink = props => {
-	const { id } = props
-	return <Link to={`${ROUTES.ADMIN_USERS}${id}`}>{`User ID: ${id}`}</Link>
-}
+export const AdminUserDetailLink = ({id, children}) => <Link to={`${ROUTES.ADMIN_USERS}${id}`}>{children}</Link>
 
-export const AdminPostDetailLink = props => {
-	const { id } = props
-	return <Link to={`${ROUTES.ADMIN_POSTS}${id}`}>{`Post ID: ${id}`}</Link>
-}
-export const AdminContestDetailLink = props => {
-	const { id } = props
-	return <Link to={`${ROUTES.ADMIN_CONTESTS}${id}`}>{props.children}</Link>
-}
+export const AdminPostDetailLink = ({id}) =>  <Link to={`${ROUTES.ADMIN_POSTS}${id}`}>{`Post ID: ${id}`}</Link>
+
+export const AdminContestDetailLink = ({id, children}) =><Link to={`${ROUTES.ADMIN_CONTESTS}${id}`}>{children}</Link>
