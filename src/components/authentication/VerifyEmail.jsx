a7a85cd88445e8ab01e983caf447a854867bc578
John Moen <entity.john@gmail.com>
2019-10-01 09:29:17 -0500

    improve email verify page

--- src/components/authentication/VerifyEmail.jsx
+++ src/components/authentication/VerifyEmail.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
+import { Container, Button } from 'reactstrap'
 import { useFirebaseContext } from '../../contexts/useFirebaseContext'
 
 export default function VerifyEmail() {
-	const [isSent, setIsSent] = useState(false)
-	const firebaseApp = useFirebaseContext()
-	const sendEmail = event => {
-		event.preventDefault()
-		firebaseApp.doSendEmailVerification()
-		setIsSent(true)
-	}
-	return isSent ? (
-		<div>Verification Email Sent!</div>
-	) : (
-		<div>
-			<button onClick={sendEmail}>Send Verification Email</button>
-		</div>
-	)
+  const [isSent, setIsSent] = useState(false)
+  const firebaseApp = useFirebaseContext()
+  const sendEmail = event => {
+    event.preventDefault()
+    firebaseApp.doSendEmailVerification()
+    setIsSent(true)
+  }
+  return (
+    <Container>
+      {isSent ? (
+        <h4>Verification Email Sent!</h4>
+      ) : (
+          <>
+            <p>
+              In order to allow access to your account information, we just need
+              to make sure it's you!
+          </p>
+            <p>
+              Send yourself an email from us and follow the link back to here!
+          </p>
+            <Container>
+              <Button color='primary' size='lg' onClick={sendEmail}>
+                Send Verification Email
+            </Button>
+            </Container>
+          </>
+        )}
+    </Container>
+  )
 }
