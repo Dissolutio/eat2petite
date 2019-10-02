80d9ba941020d302dfc674ea5e423a7bba8b688b
John Moen <entity.john@gmail.com>
2019-10-02 13:21:37 -0500

    finish create contest form
    It generates enough inputs for the number of challenges, and now builds an orderOfChallenges object like {1: challenge.uid, 2: otherChallenge.uid ... }

--- src/components/admin/CreateContestForm.jsx
+++ src/components/admin/CreateContestForm.jsx
@@ -9,24 +9,42 @@ export default function ContestCreateForm() {
 	const { user } = useAuthUserContext()
 	const { appData, createContest, enrollUserInContest } = useDataContext()
 	const { users, posts, contests, challenges } = appData
+	const [numberOfChallenges, setNumberOfChallenges] = React.useState(6)
+	const generateChallengeOrderInputLength = () => {
+		var array = [];
+		for (var i = 1; i <= numberOfChallenges; i++) {
+			array.push(i);
+		}
+		return array
+	}
+	const challengeInputNames = generateChallengeOrderInputLength().map((input, index) => `Challenge${index + 1}`)
 	const title = useInputValue('Sample1')
-
+	const daysPerChallenge = useInputValue(14)
 	const createContestOnSubmit = async event => {
 		event.preventDefault()
+		const orderOfChallenges = () => {
+			return challengeInputNames.reduce((orderOfChallenges, inputName, currIndex) => {
+				orderOfChallenges[currIndex + 1] = event.target[inputName].value
+				return orderOfChallenges
+			}, {})
+		}
 		const enrolledUsers = [...event.target.enrolledUsers].filter(input => input.checked).map(input => input.value)
 		let newContest = {
 			title: title.value,
 			startDate: event.target.startDate.value,
-			enrollmentCap: event.target.enrollmentCap.value,
+			daysPerChallenge: daysPerChallenge.value,
+			orderOfChallenges: orderOfChallenges(),
+			numberOfChallenges,
 		}
 		console.log('new Contest:', newContest)
-		const newContestId = await createContest(newContest)
-		if (newContestId) {
-			enrolledUsers.forEach(userId => {
-				console.log(newContestId)
-				enrollUserInContest(userId, newContestId)
-			})
-		}
+		// const newContestId = await createContest(newContest)
+
+		// if (newContestId) {
+		// 	enrolledUsers.forEach(userId => {
+		// 		console.log(newContestId)
+		// 		enrollUserInContest(userId, newContestId)
+		// 	})
+		// }
 	}
 	const today = moment().format('YYYY-MM-DD')
 	return (
@@ -42,8 +60,12 @@ export default function ContestCreateForm() {
 					<Input name="startDate" type="date" defaultValue={today} />
 				</FormGroup>
 				<FormGroup>
-					<Label for="enrollmentCap">Enrollment Cap</Label>
-					<Input name="enrollmentCap" type="number" defaultValue="Enrollment cap" />
+					<Label for="numberOfChallenges">Number of Challenges</Label>
+					<Input name="numberOfChallenges" type="number" value={numberOfChallenges} onChange={(event) => setNumberOfChallenges(event.target.value)} />
+				</FormGroup>
+				<FormGroup>
+					<Label for="daysPerChallenge">Days per Challenge</Label>
+					<Input name="daysPerChallenge" type="number" {...daysPerChallenge} />
 				</FormGroup>
 				{users && (
 					<FormGroup>
@@ -51,16 +73,34 @@ export default function ContestCreateForm() {
 						{Object.keys(users).filter(userKey => users[userKey].userRole === 'default').map(userKey => {
 							const user = users[userKey]
 							return (
-								<FormGroup key={userKey} check inline>
+								<FormGroup key={userKey} check >
 									<Label check>
-										{`${user.firstName} ${user.lastName}`}
 										<Input type="checkbox" value={userKey} name="enrolledUsers" />
+										{`${user.firstName} ${user.lastName}`}
 									</Label>
 								</FormGroup>
 							)
 						})}
 					</FormGroup>
 				)}
+				{
+					generateChallengeOrderInputLength().map((orderSelect, orderIndex) => {
+						const inputName = `Challenge${orderIndex + 1}`
+						return (
+							<FormGroup key={inputName}>
+								<Label for={inputName}>{`Challenge ${orderIndex + 1}`}</Label>
+								<Input type="select" name={inputName} id="exampleSelect">
+									{
+										challenges && Object.keys(challenges).map(challengeKey => {
+											const challenge = challenges[challengeKey]
+											return (<option key={`${orderIndex}--${challengeKey}`} value={challenge.uid}>{challenge.challengeName}</option>)
+										})
+									}
+								</Input>
+							</FormGroup>
+						)
+					})
+				}
 				<Button type="submit">Submit</Button>
 			</Form>
 		</Container>
