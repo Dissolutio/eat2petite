3c42c12d385b8218e7e0298fb9c03fc68e2fc9cf
John Moen <entity.john@gmail.com>
2019-10-10 16:09:54 -0500

    style landing page

--- src/components/layout/LandingPage.jsx
+++ src/components/layout/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container } from 'reactstrap'
 import HeroImage from './HeroImage'
+import styled from 'styled-components'
+
 export default function LandingPage() {
 	return (
 		<>
@@ -8,25 +10,42 @@ export default function LandingPage() {
 			<Container>
 				<h2 className='text-center'>About Eat-2-Petite</h2>
 				<p>
-					Eat 2 Petite was born from a simple thought that most people experience. <strong>I need to lose weight, but I cannot stop eating!</strong> I wish I could just eat my way to thin. 
+					Eat 2 Petite was born from a simple thought:
 					</p>
-					<p>
-					However, not everyone wants to lose weight. Some are content with their weight, some want to gain weight, some just want more energy, and some want something new. The nutritional coaching services that Eat 2 Petite provides will help with any of these goals. It is tinkered and tweaked to be what you desire. A personal coach will discuss your goals, preferences, and habits to make your meal plans.
-				</p>
+				<Blockquote>
+					I need to lose weight, but I cannot stop eating!
+					<span>-Everybody</span>
+				</Blockquote>
 				<p>
-					Eat 2 Petite is not a one-size-fits-all diet plan. Here, we believe that every person’s life, environment, tastes, and economic situations are unique. One diet that may work well for the at-home-mom that loves to cook may not work for the traveling male executive that lives on lunch and dinner meetings.
-				</p>
-				<p>
-					Eat 2 Petite wants to teach you about nutrition and how to navigate around unhealthy habits. Founded by Jennifer Dotson, a mother of 3 boys, a wife, a student,  a surgical technologist who specializes in cardiac surgery, and now a business owner. Her many roles in life have given her the ability to relate to a variety of people and  achieve success in individualized diet plans.
+					I wish I could just eat my way to thin.
 					</p>
-					<p>
-					Eat 2 Petite strives to simplify and clarify the world of nutrition while educating the community through sharing blogs, vlogs, and recipes. Follow Eat 2 Petite in the learning and coaching that Jennifer implements in her clients’ lives.
-				</p>
-				<h2>About Jennifer Dotson</h2>
 				<p>
-				Jennifer is a mother of three boys, an 18-year-old recent high school graduate, a 12-year-old 7th grader, and a 4-year-old rowdy preschooler. She has been married to her husband, Nathan, for eleven years and counting. She and Nathan started a janitorial cleaning company after she received her Associates of Arts in General Studies.  She became a surgical technologist five years after the business had become sustainable. Then she drafted to the cardiovascular/cardiothoracic surgical team upon graduation. Making routine trips to College Station she slowly got acquainted with A&M’s campus. After attending and graduating A&M, she rejoined her surgical team, and founded Eat-2-Petite. Her hobbies consist of reading, baking, cooking, crafting, hunting, and outdoor obstacle races.
+					Eat 2 Petite is not a one-size-fits-all diet plan. Here, we believe that every person’s life, environment, tastes, and economic situations are unique.
+				</p>
+				<p>
+					Eat 2 Petite wants to teach you about nutrition and how to navigate around unhealthy habits.
+					</p>
+				<h2 className='text-center'>About the coach</h2>
+				<p>
+					Jennifer Dotson has a passion for keeping people healthy. From the operating room to her consulting services, she has immersed herself in a world where sharing her knowledge, skills, and just a little push, she can help people eat their way to a healthy life.
 					</p>
 			</Container>
 		</>
 	)
 }
+const Blockquote = styled.blockquote`
+	font-size: 1.2rem;
+	margin:50px auto;
+	color: #555555;
+	padding:1.2rem;
+	border-left:8px solid #78C0A8 ;
+	position: relative;
+	background:#EDEDED;
+	span {
+	display: block; 
+	color: #333333; 
+	font-style: normal; 
+	font-weight: bold; 
+	margin-top:1em;
+}
+`
