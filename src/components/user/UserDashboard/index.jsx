db2f62a1e4293bae4bb29249e1283b67b9310bfb
John Moen <entity.john@gmail.com>
2019-10-18 23:52:30 -0500

    build out UserDashboard

--- src/components/user/UserDashboard/index.jsx
+++ src/components/user/UserDashboard/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Container } from 'reactstrap'
-import { eachDayOfInterval, addDays, isAfter } from 'date-fns'
+import { format, eachDayOfInterval, addDays, isAfter, differenceInDays } from 'date-fns'
 
 import { useDataContext } from '../../../contexts/useDataContext'
 import { useAuthUserContext } from '../../../contexts/useAuthUserContext'
 
 import { UserDashboardCalendar } from './UserDashboardCalendar'
 import UserContestsList from '../UserContestsList'
+import WaterChallengePostForm from '../WaterChallengePostForm'
 
 function sortByMostCurrentStartDate(a, b) {
     if (isAfter(new Date(a.startDate), new Date(b.startDate))) {
@@ -17,14 +18,15 @@ function sortByMostCurrentStartDate(a, b) {
 }
 export const UserDashboard = () => {
     const { appData } = useDataContext()
-    const { contests } = appData
+    const { contests, posts } = appData
     const { user } = useAuthUserContext()
     const userEnrolledContests = user.contests && Object.keys(user.contests).map(contestKey => (contests[contestKey]))
+    console.log("TCL: UserDashboard -> userEnrolledContests", userEnrolledContests)
     const [userSelectedContest, setUserSelectedContest] = React.useState()
     const [selectedDate, setSelectedDate] = React.useState(new Date())
     const [hasFiredAutoSelect, setHasFiredAutoSelect] = React.useState(false)
 
-    const sortedByMostRecent = [...userEnrolledContests.sort(sortByMostCurrentStartDate)]
+    const sortedByMostRecent = userEnrolledContests && [...userEnrolledContests.sort(sortByMostCurrentStartDate)]
     const autoSelectedContest = userEnrolledContests && sortedByMostRecent[0]
     if (autoSelectedContest && hasFiredAutoSelect === false) {
         setHasFiredAutoSelect(true)
@@ -33,17 +35,35 @@ export const UserDashboard = () => {
     if (userSelectedContest) {
         const { daysPerChallenge, numberOfChallenges } = userSelectedContest
         const contestLengthInDays = daysPerChallenge * numberOfChallenges
-        const startDate = new Date(userSelectedContest.startDate)
-        const endDate = new Date(addDays(startDate, contestLengthInDays))
-        // const allContestDays = eachDayOfInterval({ start: startDate, end: endDate })
+        const contestStartDate = new Date(userSelectedContest.startDate)
+        const contestEndDate = new Date(addDays(contestStartDate, contestLengthInDays))
+        const getPostForDay = () => {
+            return
+        }
+        const allContestDays = eachDayOfInterval({ start: contestStartDate, end: contestEndDate })
+        const challengeForDay = () => {
+            const daysStartToPostDate = differenceInDays(selectedDate, contestStartDate)
+            console.log("TCL: challengeForDay -> daysStartToPostDate", daysStartToPostDate)
+            return daysStartToPostDate
+        }
+        console.log("TCL: UserDashboard -> challengeForDay()", challengeForDay())
         return (
             <Container>
-                {userSelectedContest.startDate && <UserDashboardCalendar
-                    startDate={startDate}
-                    endDate={endDate}
-                    selectedDate={selectedDate}
+                {userSelectedContest.startDate &&
+                    <UserDashboardCalendar
+                        selectedDate={selectedDate}
+                        setSelectedDate={setSelectedDate}
+                        startDate={contestStartDate}
+                        endDate={contestEndDate}
+                    />}
+                <WaterChallengePostForm
+                    userSelectedContest={userSelectedContest}
+                    selectedDate={format(new Date(selectedDate), 'yyyy-MM-dd')}
                     setSelectedDate={setSelectedDate}
-                />}
+                    contestStartDate={contestStartDate}
+                    contestEndDate={contestEndDate}
+                />
+                <UserContestsList contests={contests} />
             </Container>
         )
     }
