af7b43626227080a38c56e775b5e36c04a6f6815
John Moen <entity.john@gmail.com>
2019-10-18 23:51:54 -0500

    build out water challenge post form

--- src/components/user/WaterChallengePostForm.jsx
+++ src/components/user/WaterChallengePostForm.jsx
@@ -1,34 +1,45 @@
 import React from 'react'
 import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap'
-// import { useDataContext } from '../../contexts/useDataContext'
+import { useDataContext } from '../../contexts/useDataContext'
 import { useAuthUserContext } from '../../contexts/useAuthUserContext'
-const moment = require('moment')
-
-export default function WaterChallengePostForm() {
-	// const { createUserPost } = useDataContext()
+import { format, addDays, isWithinInterval } from 'date-fns'
+export default function WaterChallengePostForm(props) {
+	const { createUserPost } = useDataContext()
 	const { user } = useAuthUserContext()
+	const { selectedDate, setSelectedDate, contestStartDate, contestEndDate, userSelectedContest } = props
+	console.log("TCL: WaterChallengePostForm -> userSelectedContest", userSelectedContest)
+
+	const dateChangeHandler = (event) => {
+		// add one day, because the date input rounds downa day for some reason
+		const newDate = addDays(new Date(event.target.value), 1)
+		if (isWithinInterval(newDate, { start: contestStartDate, end: contestEndDate })) {
+			setSelectedDate(newDate)
+		}
+	}
 	const createPostOnSubmit = event => {
 		event.preventDefault()
-		console.log(event.target)
 		let newPost = {
 			author: user.uid,
 			createdAt: new Date(),
+			postDate: selectedDate,
+			contestId: userSelectedContest.uid,
 			postData: {
 				quantity: event.target.quantity.value || 0,
 				quantityUnits: event.target.quantityUnits.value,
 			},
 		}
-		console.log('new Post:', newPost)
+		console.log("TCL: newPost", newPost)
 		// createUserPost(newPost)
 	}
-	const today = moment().format('YYYY-MM-DD')
+	const today = format(new Date(), 'yyyy-MM-dd')
 	return (
 		<Container>
 			<Form onSubmit={createPostOnSubmit} className="border border-primary rounded p-4 mt-4 mb-3 text-center">
 				<h2>Water Challenge Post Form</h2>
 				<FormGroup>
 					<Label for="postDate">Post Date</Label>
-					<Input name="postDate" type="date" defaultValue={today} />
+					<Input name="postDate" type="date"
+						onChange={dateChangeHandler} value={selectedDate} />
 				</FormGroup>
 				<FormGroup>
 					<Label for="quantity">Quantity</Label>
