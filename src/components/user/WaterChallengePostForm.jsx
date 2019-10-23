abf35a71d62c24c2dbbbc11e29b8cc7a56411863
John Moen <entity.john@gmail.com>
2019-10-23 09:13:44 -0500

    refactor and style water input form

--- src/components/user/WaterChallengePostForm.jsx
+++ src/components/user/WaterChallengePostForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import { Container, Button, Form, FormGroup, Label, Input, InputGroup, InputGroupAddon } from 'reactstrap'
 import { useDataContext } from '../../contexts/useDataContext'
 import { useAuthUserContext } from '../../contexts/useAuthUserContext'
 import { format, addDays, isWithinInterval } from 'date-fns'
+import useInputValue from '../../modules/hooks/useInputValue'
+
 export default function WaterChallengePostForm(props) {
-  const { createUserPost } = useDataContext()
+  const { saveUserPost } = useDataContext()
   const { user } = useAuthUserContext()
   const {
     selectedDate,
@@ -14,19 +16,22 @@ export default function WaterChallengePostForm(props) {
     userSelectedContest,
   } = props
 
+  const contestStartDateText = format(contestStartDate, 'LLL d')
+  const todaysDateText = format(new Date(), 'LLL d')
   const dateChangeHandler = (event) => {
     // add one day, because the date input rounds downa day for some reason
     const newDate = addDays(new Date(event.target.value), 1)
     if (
       isWithinInterval(newDate, {
         start: contestStartDate,
-        end: contestEndDate,
+        end: new Date(),
       })
     ) {
       setSelectedDate(newDate)
     }
   }
-  const createPostOnSubmit = (event) => {
+  const quantityDrank = useInputValue(0)
+  const onSubmitForm = (event) => {
     event.preventDefault()
     let newPost = {
       author: user.uid,
@@ -38,17 +43,17 @@ export default function WaterChallengePostForm(props) {
       quantityDrankUnits: event.target.quantityUnits.value,
     }
     console.log('TCL: newPost', newPost)
-    // createUserPost(newPost)
+    // saveUserPost(newPost)
   }
-  const today = format(new Date(), 'yyyy-MM-dd')
   return (
     <Container>
       <Form
-        onSubmit={createPostOnSubmit}
+        onSubmit={onSubmitForm}
         className="border border-primary rounded p-4 mt-4 mb-3 text-center">
-        <h2>Water Challenge Post Form</h2>
+        <h5 className='text-primary border-bottom border-primary'>Water Challenge</h5>
         <FormGroup>
           <Label for="postDate">Post Date</Label>
+          <span className='small d-block text-muted'>{`${contestStartDateText} - ${todaysDateText}`}</span>
           <Input
             name="postDate"
             type="date"
@@ -56,18 +61,20 @@ export default function WaterChallengePostForm(props) {
             value={selectedDate}
           />
         </FormGroup>
-        <FormGroup>
-          <Label for="quantity">Quantity</Label>
-          <Input name="quantity" type="number" placeholder="quantity" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="quantityUnits">Units</Label>
-          <Input type="select" name="quantityUnits" defaultValue="cups">
+        <InputGroup size="sm">
+          <Label for="quantity" hidden>Quantity</Label>
+          <InputGroupAddon addonType="prepend">Quantity</InputGroupAddon>
+          <Input name="quantity" type="number" bsSize='sm' {...quantityDrank} />
+        </InputGroup>
+        <InputGroup size="sm">
+          <Label for="quantityUnits" hidden>Units</Label>
+          <InputGroupAddon addonType="prepend">Units</InputGroupAddon>
+          <Input type="select" name="quantityUnits" defaultValue="cups" bsSize='sm'>
             <option value="cups">Cups</option>
             <option value="ounces">Ounces</option>
             <option value="liters">Liters</option>
           </Input>
-        </FormGroup>
+        </InputGroup>
         <Button type="submit">Submit</Button>
       </Form>
     </Container>
