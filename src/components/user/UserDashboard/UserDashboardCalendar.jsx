27e0e5e301e68a4ee1c4b2ee0e6ca8c8ad9d48ff
John Moen <entity.john@gmail.com>
2019-10-27 23:24:13 -0500

    refactor Calendar component

--- src/components/user/UserDashboard/UserDashboardCalendar.jsx
+++ src/components/user/UserDashboard/UserDashboardCalendar.jsx
@@ -2,33 +2,17 @@ import React from 'react'
 import Calendar from 'react-calendar'
 
 
-export const UserDashboardCalendar = props => {
-    const { startDate, contestEndDate, selectedDate, setSelectedDate } = props
-    const changeHandler = date => {
-        setSelectedDate(date)
-    }
-    return (
-        <Calendar
-            onChange={changeHandler}
-            value={selectedDate}
-            calendarType="US"
-            minDate={startDate}
-            maxDate={contestEndDate}
-            minDetail={'month'}
-        />
-    )
-const UserDashboardCalendar = (props) => {
-  const { startDate, endDate, selectedDate, setSelectedDate } = props
-  const changeHandler = (date) => {
-    setSelectedDate(date)
-  }
+const UserDashboardCalendar = props => {
+  const { startDate, contestEndDate, selectedDate, setSelectedDate } = props
+  const changeHandler = date => setSelectedDate(date)
   return (
     <Calendar
       onChange={changeHandler}
       value={selectedDate}
       calendarType="US"
       minDate={startDate}
-      maxDate={endDate}
+      maxDate={contestEndDate}
+      minDetail={'month'}
     />
   )
 }
