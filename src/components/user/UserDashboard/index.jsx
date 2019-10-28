b15ec535b422b480532c8deee3ac81999576e819
John Moen <entity.john@gmail.com>
2019-10-27 23:23:06 -0500

    implement query-params to query which contest to show in dash

--- src/components/user/UserDashboard/index.jsx
+++ src/components/user/UserDashboard/index.jsx
@@ -4,36 +4,32 @@ import { sortByMostCurrentStartDate } from './utils'
 import { useDataContext } from '../../../contexts/useDataContext'
 import UserContestDashboard from './UserContestDashboard'
 import UserContestsList from '../UserContestsList'
+const queryString = require('query-string')
 
 export const UserHomepage = (props) => {
   const [userSelectedContest, setUserSelectedContest] = React.useState()
-  const [hasFiredAutoSelect, setHasFiredAutoSelect] = React.useState(false)
+  const [hasInitialized, setHasInitialized] = React.useState(false)
 
-  const { appData, createUserPost } = useDataContext()
+  const { appData } = useDataContext()
   const { contests, posts, me } = appData
-  const { contestChosen } = props
-  const userEnrolledContests =
-    me.contests &&
-    Object.keys(me.contests).map((contestKey) => contests[contestKey])
-
-  if (contestChosen && !hasFiredAutoSelect) {
-    setHasFiredAutoSelect(true)
-    setUserSelectedContest(contestChosen)
-  }
-
-  const sortedByMostRecent = userEnrolledContests && [
-    ...userEnrolledContests.sort(sortByMostCurrentStartDate),
-  ]
+  const queryParams = queryString.parse(props.location.search)
+  const userEnrolledContests = me.contests && Object.keys(me.contests).map((contestKey) => contests[contestKey])
+  const sortedByMostRecent = userEnrolledContests && [...userEnrolledContests.sort(sortByMostCurrentStartDate)]
   const autoSelectedContest = userEnrolledContests && sortedByMostRecent[0]
-
-  if (autoSelectedContest && !hasFiredAutoSelect && !contestChosen) {
-    setHasFiredAutoSelect(true)
-    setUserSelectedContest(autoSelectedContest)
+  const queryContest = queryParams.selectedContest && contests[queryParams.selectedContest]
+  if (!hasInitialized) {
+    if (queryContest) {
+      setUserSelectedContest(queryContest)
+    } else if (autoSelectedContest) {
+      setUserSelectedContest(autoSelectedContest)
+    }
+    setHasInitialized(true)
   }
+
   if (userSelectedContest) {
     return (
       <Container>
-        <UserContestDashboard userSelectedContest={userSelectedContest} />
+        <UserContestDashboard me={me} userSelectedContest={userSelectedContest} posts={posts} />
         <UserContestsList
           userEnrolledContests={userEnrolledContests}
           userSelectedContest={userSelectedContest}
@@ -46,7 +42,7 @@ export const UserHomepage = (props) => {
     <Container className="text-center">
       <h1 className="text-center">User Dashboard</h1>
       <hr />
-      <UserContestsList contests={contests} userSelectedContest={userSelectedContest} />
+      <UserContestsList contests={contests} userSelectedContest={userSelectedContest} setUserSelectedContest={setUserSelectedContest} />
     </Container>
   )
 }
