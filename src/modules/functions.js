26be83c2e7b772ab295353a94e9d33c5e2bb8bd9
John Moen <entity.john@gmail.com>
2019-10-29 16:10:27 -0500

    add ordinal number convert function

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,4 +1,4 @@
-export const getDateArray = function(start, end) {
+export const getDateArray = function (start, end) {
 	var arr = []
 	var dt = new Date(start)
 	while (dt <= end) {
@@ -6,4 +6,19 @@ export const getDateArray = function(start, end) {
 		dt.setDate(dt.getDate() + 1)
 	}
 	return arr
+}
+
+export const ordinalSuffixOf = (i) => {
+	const j = i % 10,
+		k = i % 100;
+	if (j == 1 && k != 11) {
+		return i + "st";
+	}
+	if (j == 2 && k != 12) {
+		return i + "nd";
+	}
+	if (j == 3 && k != 13) {
+		return i + "rd";
+	}
+	return i + "th";
 }
