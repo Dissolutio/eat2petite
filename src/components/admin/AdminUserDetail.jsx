50deb450e70cab396c1cbabc8d873f97455364eb
John Moen <entity.john@gmail.com>
2019-10-30 13:34:35 -0500

    refactor and organize for user challenge target forms

--- src/components/admin/AdminUserDetail.jsx
+++ src/components/admin/AdminUserDetail.jsx
@@ -1,27 +1,47 @@
 import React from 'react'
-import { Container, ListGroup, ListGroupItem, Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Container, Card, ListGroup, ListGroupItem } from 'reactstrap'
 import { withRouter } from 'react-router-dom'
-import { useDataContext } from '../../contexts/useDataContext'
-import UserChallengeTargetsForm from './UserChallengeTargetsForm'
+import { AdminContestDetailLink } from '../layout/Links'
+import EditUserChallengeTargetsSection from './UserTargetForms/index'
+
 function AdminUserDetail(props) {
-	const { appData } = useDataContext()
-	const { users, challenges } = appData
+	const { users, challenges, contests } = props
 	const { id } = props.match.params
 	const user = users && users[id]
-	return (
-		(user && (
-			<Container key={user.uid}>
-				<h3>{`${user.firstName} ${user.lastName}`}</h3>
-				<ListGroup>
-					<ListGroupItem>{`Username: ${user.username}`}</ListGroupItem>
-					<ListGroupItem>{`Email: ${user.email}`}</ListGroupItem>
-					<ListGroupItem>{`Weight: ${user.userWeight} lbs`}</ListGroupItem>
-					<ListGroupItem>{`Height: ${user.userHeightFeet}' ${user.userHeightInches}"`}</ListGroupItem>
-					<UserChallengeTargetsForm user={user} challenges={challenges} />
-				</ListGroup>
+
+	if (user) {
+		const { uid, firstName, lastName, username, email, userWeight, userHeightFeet, userHeightInches } = user
+		const userContests = Object.keys(user.contests)
+		return (
+			<Container>
+				<Card className='p-3'>
+					<h3 className='text-primary'>{`${firstName} ${lastName}`}</h3>
+					<div className="text-center"><small className='text-info'>{`username: ${username}`}</small></div>
+					<div className="text-center"><small className='text-secondary'>{email}</small></div>
+					<div className="text-center"><small className='text-secondary'>{`Weight: ${userWeight} lbs`}</small></div>
+					<div className="text-center"><small className='text-secondary'>{`Height: ${userHeightFeet}' ${userHeightInches}"`}</small></div>
+				</Card>
+				<Card className='p-4'>
+					<ListGroup>
+						<h5>User's contests:</h5>
+						{userContests && userContests.map(contestId => {
+							const contest = contests[contestId]
+							return (
+								<ListGroupItem key={contestId}>
+									<AdminContestDetailLink id={contestId}>
+										{`${contest.title}`}
+									</AdminContestDetailLink>
+								</ListGroupItem>
+							)
+						})}
+					</ListGroup>
+				</Card>
+				<EditUserChallengeTargetsSection user={user} challenges={challenges} />
 			</Container>
-		)) || <Container>No User Found</Container>
-	)
+		)
+	} else {
+		return (<Container>No User Found</Container>)
+	}
 }
 
 export default withRouter(AdminUserDetail)
