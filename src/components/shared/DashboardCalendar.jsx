bdeaba0a32f891755a7d39f9e14c0eddadf4d7ed
John Moen <entity.john@gmail.com>
2019-11-10 19:48:54 -0600

    style calendar

--- src/components/shared/DashboardCalendar.jsx
+++ src/components/shared/DashboardCalendar.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
 import Calendar from 'react-calendar'
+import styled from 'styled-components'
+import { format } from 'date-fns'
 
-
-const UserDashboardCalendar = props => {
+const DashboardCalendar = props => {
   const { minDate, maxDate, selectedDate, dateChangeHandler } = props
+  const arrayOfFormattedDatesToHighlight = [format(new Date(), 'MMMM d, yyyy'), 'November 7, 2019', 'October 30, 2019']
   return (
-    <Calendar
-      onChange={dateChangeHandler}
-      value={selectedDate}
-      calendarType="US"
-      minDate={minDate}
-      maxDate={maxDate}
-      minDetail={'month'}
-    />
+    <Highlighter arrayOfFormattedDatesToHighlight={arrayOfFormattedDatesToHighlight}>
+      <RestyledCalendar
+        onChange={dateChangeHandler}
+        value={selectedDate}
+        calendarType="US"
+        minDate={minDate}
+        maxDate={maxDate}
+        minDetail={'month'}
+        tileClassName={'relative-position-tiles'}
+      />
+    </Highlighter>
   )
 }
-export default UserDashboardCalendar
+export default DashboardCalendar
+const RestyledCalendar = styled(Calendar)`
+.react-calendar__month-view__days__day--weekend {
+  color: inherit;
+}
+.react-calendar__month-view__days__day--neighboringMonth {
+  color: inherit;
+}
+button:disabled {
+  background-color: var(--gray4);
+  color: var(--gray4);
+}
+`
+const Highlighter = styled.div`
+${props => props.arrayOfFormattedDatesToHighlight ?
+    `.relative-position-tiles {
+  position: relative;
+}` : ``}
+${props => props.arrayOfFormattedDatesToHighlight &&
+    props.arrayOfFormattedDatesToHighlight.reduce((result, date) => {
+      return result + `abbr[aria-label = "${date}"] {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        padding: 35% 0;
+        background-color: var(--E2P-orange);
+      }`
+    }, '')}
+  // selected tile should still have unique color
+  .react-calendar__tile--active > abbr {
+    background-color: inherit !important;
+  }
+`
