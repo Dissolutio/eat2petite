c21ba0feb83e811d4c5716a9cc57ed8465702a32
John Moen <entity.john@gmail.com>
2019-11-13 09:06:18 -0600

    wip AdminPostsList

--- src/components/admin/AdminPostsList.jsx
+++ src/components/admin/AdminPostsList.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { AdminPostDetailLink } from '../layout/Links'
 
 export default function AdminPostsList(props) {
 	const { posts } = props
+	if (!posts) { return 'No posts found!' }
+	const userIdsArr = Object.keys(posts)
+	const postsByUserId = Object.values(posts)
+	const allPosts = userIdsArr.reduce((endArr, userId) => {
+		return endArr
+	}, [])
 	return (
 		<ul>
 			<h2>Posts List</h2>
-			{posts ? Object.values(posts).map(post => (
+			{allPosts && allPosts.map(post => (
 				<li key={post.uid}>
 					<ul>
 						<AdminPostDetailLink postId={post.uid} />
-							<Link to={`/user/admin/posts/${post.uid}`}>{`uid: ${post.uid}`}</Link>
-						</li>
-						<AdminPostDetailLink id={post.uid} />
 						<li>{`contestId: ${post.contestId}`}</li>
 						<li>{`userId: ${post.userId}`}</li>
 						<li>{`challengeId: ${post.challengeId}`}</li>
 						<li>{`postDate: ${post.postDate}`}</li>
-						{/* <li>{post.postData}</li> */}
 					</ul>
 				</li>
 			))
-				: 'No Posts Found'
 			}
 		</ul>
 	)
