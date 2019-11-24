3fc044e376da15a668160b3214408b03cdb47232
John Moen <entity.john@gmail.com>
2019-11-24 15:31:32 -0600

    add theme bg color

--- src/components/layout/StyleWrappers.jsx
+++ src/components/layout/StyleWrappers.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export const AppStyle = styled.div`
 	min-height: 100%;
 	color: var(--font-dark);
-	background-color: #d5fefd;
+	background-color: var(--app-bg);
 background-image: linear-gradient(350deg, var(--white), var(--gray1));
 `
 
