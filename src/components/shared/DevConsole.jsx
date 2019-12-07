f16d4fd0ef8205defb65587f63852272afa03ecb
John Moen <entity.john@gmail.com>
2019-12-07 14:55:15 -0600

    cleanup file, remove unused component UserDevConsole

--- src/components/shared/DevConsole.jsx
+++ src/components/shared/DevConsole.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Container, ButtonGroup, Button, } from 'reactstrap'
-import { random } from 'lodash'
-import { format, addDays, eachDayOfInterval, isSameDay } from 'date-fns'
+
 import { useFirebaseContext } from '../../contexts/useFirebaseContext'
 import { useDataContext } from '../../contexts/useDataContext'
 
@@ -28,60 +27,6 @@ export function AdminDevConsole() {
 				/>
 				<DoubleClickButton doubleClickCallback={dbResetToSample} text="DB RESET TO SAMPLE"
 					firstColor="danger" secondColor="danger"
-				/>
-			</ButtonGroup>
-		</Container>
-	)
-}
-
-export function UserDevConsole(props) {
-	const { userSelectedContest, currentChallenge } = props
-	const {
-		appData,
-		savePost,
-		loadFirebaseData,
-		consoleLogAppData,
-	} = useDataContext()
-	const { me } = appData
-
-	const createABunchOfWaterChallengePosts = (event) => {
-		const yesterday = new Date(format(addDays(new Date(), -1), 'P'))
-		const dateInterval = eachDayOfInterval({
-			start: new Date(userSelectedContest.startDate),
-			end: yesterday,
-		})
-		Promise.all(dateInterval.map(dateToPost => {
-			const createdAt = (new Date()).toString()
-			const postDate = format(dateToPost, 'P')
-			const checkedInBonus = isSameDay(new Date(createdAt), new Date(postDate))
-			let newPost = {
-				author: me.uid,
-				userId: me.uid,
-				uid: null,
-				contestId: userSelectedContest.uid,
-				challengeId: currentChallenge.uid,
-				postDate,
-				createdAt,
-				quantityWaterDrank: random(1, 10),
-				quantityWaterDrankUnits: 'cups',
-				checkedInBonus,
-				lastEditedAt: createdAt,
-			}
-			return savePost(newPost)
-		})).then(() => (
-			console.log('SamplePostsMade')
-		))
-	}
-	return (
-		<Container className="border border-secondary p-3 m-2">
-			<h4 className="text-center">Dev Console</h4>
-			<ButtonGroup vertical style={{ width: '100%' }}>
-				<Button color="primary" onClick={consoleLogAppData}>
-					CONSOLE LOG APP DATA
-			</Button>
-				<DoubleClickButton doubleClickCallback={loadFirebaseData} text="LOAD FIREBASE DATA " />
-				<DoubleClickButton doubleClickCallback={createABunchOfWaterChallengePosts} text="MAKE ALL PREVIOUS POSTS FOR USER IN SELECTED CONTEST "
-					firstColor="primary" secondColor="warning"
 				/>
 			</ButtonGroup>
 		</Container>
