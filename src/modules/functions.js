294a3ab84c4645ab7068159da96d17eb6ffadadb
John Moen <entity.john@gmail.com>
2019-12-09 13:30:14 -0600

    add two functions: flatten, buildNewTargetsFromEvent

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,4 +1,4 @@
-import { isAfter, format } from 'date-fns'
+import { isAfter } from 'date-fns'
 
 export const ordinalSuffixOf = (i) => {
 	const j = i % 10,
@@ -23,4 +23,53 @@ export function sortByMostCurrentStartDate(a, b) {
 }
 export function lastInitial(string) {
 	return string.split('')[0].toUpperCase() + '.'
+}
+
+export function flatten(objectToFlatten) {
+	return Object.values(objectToFlatten).reduce((reduction, current) => {
+		return {
+			...reduction,
+			...current,
+		}
+	}, {})
+}
+
+export function buildNewTargetsFromEvent(event) {
+	return {
+		challenge1: {
+			quantityWaterDrank:
+				event.target.quantityWaterDrank && event.target.quantityWaterDrank.value,
+			quantityWaterDrankUnits:
+				event.target.quantityWaterDrankUnits && event.target.quantityWaterDrankUnits.value,
+		},
+		challenge2: {
+			servingsVegetablesEaten:
+				event.target.servingsVegetablesEaten && event.target.servingsVegetablesEaten.value,
+		},
+		challenge3: {
+			proteinConsumed: event.target.proteinConsumed && event.target.proteinConsumed.value,
+		},
+		challenge4: {
+			lightExcerciseDuration:
+				event.target.lightExcerciseDuration && event.target.lightExcerciseDuration.value,
+			mediumExcerciseDuration:
+				event.target.mediumExcerciseDuration && event.target.mediumExcerciseDuration.value,
+			heavyExcerciseDuration:
+				event.target.heavyExcerciseDuration && event.target.heavyExcerciseDuration.value,
+		},
+		challenge5: {
+			refinedCarbsConsumed:
+				event.target.refinedCarbsConsumed && event.target.refinedCarbsConsumed.value,
+		},
+		challenge6: {
+			quantitySugarConsumed:
+				event.target.quantitySugarConsumed && event.target.quantitySugarConsumed.value,
+			quantitySugarConsumedUnits:
+				event.target.quantitySugarConsumedUnits && event.target.quantitySugarConsumedUnits.value,
+			quantitySaltConsumed:
+				event.target.quantitySaltConsumed && event.target.quantitySaltConsumed.value,
+			quantitySaltConsumedUnits:
+				event.target.quantitySaltConsumedUnits && event.target.quantitySaltConsumedUnits.value,
+		},
+	}
 }
