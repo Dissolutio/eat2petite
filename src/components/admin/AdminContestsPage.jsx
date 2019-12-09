5482cc50e09f43252c9f1b823ff700a4231967c4
John Moen <entity.john@gmail.com>
2019-12-09 10:28:18 -0600

    rename file

---
+++ src/components/admin/AdminContestsPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button, ListGroup, ListGroupItem } from 'reactstrap'
+import { AdminContestDetailLink, AdminCreateContestLink } from '../layout/Links'
+
+export default function AdminContestsPage({ contests, users, challenges }) {
+  return (
+    <>
+      <AdminContestsList contests={contests} />
+      <AdminCreateContestLink>
+        <Button color='primary'>Create New Contest</Button>
+      </AdminCreateContestLink>
+    </>
+  )
+}
+function AdminContestsList(props) {
+  const { contests } = props
+  return contests ? (
+    <ListGroup>
+      <h3>Contests:</h3>
+      {contests &&
+        Object.keys(contests).map((contestKey) => {
+          const contest = contests[contestKey]
+          const { uid, title } = contest
+          return (
+            <ListGroupItem key={contestKey}>
+              <AdminContestDetailLink contestId={uid}>
+                {title}
+              </AdminContestDetailLink>
+            </ListGroupItem>
+          )
+        })}
+    </ListGroup>
+  ) : (
+    <p>No Contests Found</p>
+  )
+}
